f5252228e495079d42a7e83770c08b05f3feaf7c
Jesse Maretz <jesse.maretz@gmail.com>
2021-04-16 01:34:00 -0400

    importing task and column comps to app file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Column from './components/Column';
+import Task from './components/Task';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 function App() {
   return (
     <div>
+      <Column>
+      <Task id="task-1">
+        <p>Task 1</p>
+      </Task>
+      </Column>
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
 function Task(props) {
+    const dragStart = e => {
+        const target = e.target;
+        e.dataTransfer.setData('card_id', target.id)
+        setTimeout(() => {
+            target.style.display = "none";
+        }, 0)
+    }
+
+    const dragOver = e => {
+        e.stopPropagation();
+    }
+
     return (
         <div
             id={props.id}
